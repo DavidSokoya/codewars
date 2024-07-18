6549c9cc3eaf91c02d74e56d3e3ac6df555c1a09
davidsokoya <sokoya08@gmail.com>
2024-07-18 17:49:01 +0100

    Grasshopper - Grade book

--- app.js
+++ app.js
@@ -171,9 +171,27 @@ function minMax(arr){
 // If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
 // If he doesn't get 10 hoops, return the string "Keep at it until you get it".
 
-
 function hoopCount (n) {
-   
   return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
-  
+}
+
+// 10. 
+// Grasshopper - Grade book
+
+// DESCRIPTION:
+// Grade book
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
+
+function getGrade (s1, s2, s3) {
+  let average = (s1 + s2 + s3) / 3
+  return average >= 90 ? "A" : average >= 80 ? "B" : average >= 70 ? "C" : average >= 60 ? "D" : "F"
 }
