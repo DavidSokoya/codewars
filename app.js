1407d5b85546e6a089f393baccbad0c4eaf2e1b1
davidsokoya <sokoya08@gmail.com>
2024-07-18 17:43:15 +0100

    fake binary

--- app.js
+++ app.js
@@ -64,3 +64,30 @@ function abbrevName(name){
   return  name.split(" ").map(i => i[0].toUpperCase()).join(".")
 
 }
+
+// 5. Fake Binary
+
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+ function fakeBin(x){
+   newX = ""
+  
+ for (let i = 0; i < x.length; i++) {
+   if(Number(x[i]) < 5 ) {
+        newX += "0"
+        }
+    else {
+       newX += 1;
+    }
+   }
+
+   return newX;
+ }
+
+function fakeBin(x) {
+
+return x.split('').map(n => n < 5 ? 0 : 1).join('')
+
+}
