f3c4bd4348c2c3c145c005c29b8b16c065b65520
davidsokoya <sokoya08@gmail.com>
2024-07-18 17:38:47 +0100

    Messi goals function

---
+++ app.js
@@ -0,0 +1,13 @@
+// 1. Reverse a String
+function solution(str){
+  return str.split("").reverse().join("")
+}
+
+(solution("world"))
+
+// 2. Messi goals function
+
+  function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+}
+
