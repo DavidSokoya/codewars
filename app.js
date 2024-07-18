d3b4b36f9733fb7aac188684a80be3dc6363c058
davidsokoya <sokoya08@gmail.com>
2024-07-18 17:44:10 +0100

    Twice as old

--- app.js
+++ app.js
@@ -91,3 +91,24 @@ function fakeBin(x) {
 return x.split('').map(n => n < 5 ? 0 : 1).join('')
 
 }
+
+// 6. Twice as old
+
+// DESCRIPTION:
+// Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  // your code here
+  let sonTwiceAge = sonYearsOld * 2;
+  let yearsAgoOrAfter = dadYearsOld - sonTwiceAge
+
+  return Math.abs(yearsAgoOrAfter)
+}
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(dadYearsOld - sonYearsOld * 2)
+}
