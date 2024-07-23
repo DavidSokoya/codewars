300b82faf3db45dbcd685ffdb1f2c7ca324864fb
DavidSokoya <sokoya08@gmail.com>
2024-07-23 11:48:04 +0100

    final

--- cw-22-07-24/app.js
+++ cw-22-07-24/app.js
@@ -27,3 +27,178 @@ function stringy(size) {
   }
   return string;
 }
+
+// 3. Short Long Short
+function solution(a, b) {
+    if (a.length < b.length) {
+        return a + b + a;
+    } else {
+        return b + a + b;
+    }
+}
+
+// 4. Vowel remover
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+function shortcut (string) {
+  return   string.split('')
+  .filter(str => !'aeiou'.includes(str))
+  .join('')
+}
+
+// 5. Name Shuffler
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// Example(Input --> Output)
+
+// "john McClane" --> "McClane john"
+function nameShuffler(str){
+  return str.split(' ')[1] + " " + str.split(' ')[0]
+
+
+// 6. Exclamation marks series #1: Remove an exclamation mark from the end of string
+
+// Description:
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// "Hi!"     ---> "Hi"
+// "Hi!!!"   ---> "Hi!!"
+// "!Hi"     ---> "!Hi"
+// "!Hi!"    ---> "!Hi"
+// "Hi! Hi!" ---> "Hi! Hi"
+// "Hi"      ---> "Hi"
+
+
+function remove(string) {
+  return string.endsWith('!') ? string.slice(0, -1) : string
+}
+
+// 7. Capitalization and Mutability
+
+// DESCRIPTION:
+// Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
+
+// Unfortunately, they have now left and the code they gave you doesn't work. Fix the helper function they wrote so that it works as intended (i.e. it must make the first character in the string upper case).
+
+// The string will always start with a letter and will never be empty.
+
+// Examples:
+
+// "hello" --> "Hello"
+// "Hello" --> "Hello" (the first letter was already capitalized)
+// "a"     --> "A"
+
+
+function capitalizeWord(word) {
+  let capitalized = word[0].toUpperCase()
+  let otherWord = word.slice(1, word.length);
+  if (word[0] === capitalized ) {
+    return word;
+  } else {
+    return `${capitalized}${otherWord}`
+  }
+  
+}
+function capitalizeWord(word) {
+  return word[0].toUpperCase() + word.slice(1);
+}
+
+// 8. The Wide-Mouthed frog!
+// DESCRIPTION:
+// The wide-mouth frog is particularly interested in the eating habits of other creatures.
+
+// He just can't stop asking the creatures he encounters what they like to eat. But, then he meets the alligator who just LOVES to eat wide-mouthed frogs!
+
+// When he meets the alligator, it then makes a tiny mouth.
+
+// Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide.
+
+function mouthSize(animal) {
+  
+  return animal.toLowerCase() === 'alligator' ? "small" : 'wide';
+}
+
+// 9. Reversing Words in a String
+// You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:
+
+// As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+
+// Example (Input --> Output)
+
+// "Hello World" --> "World Hello"
+// "Hi There." --> "There. Hi"
+
+function reverse(string){
+  return string.split(' ').reverse().join(' ')
+}
+
+// 10. Multiplication table for number
+
+// DESCRIPTION:
+// Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+
+// For example, a multiplication table (string) for number == 5 looks like below:
+
+// 1 * 5 = 5
+// 2 * 5 = 10
+// 3 * 5 = 15
+// 4 * 5 = 20
+// 5 * 5 = 25
+// 6 * 5 = 30
+// 7 * 5 = 35
+// 8 * 5 = 40
+// 9 * 5 = 45
+// 10 * 5 = 50
+// P. S. You can use \n in string to jump to the next line.
+
+// Note: newlines should be added between rows, but there should be no trailing newline at the end. If you're unsure about the format, look at the sample tests.
+
+// const multiTable = (number) => {
+//   let table = '';
+  
+//   for(let i = 1; i <= 10; i++) {
+//     table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`; 
+//   }
+
+//   return table;
+// }
+// 11. String Templates - Bug Fixing #5
+// Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+
+function buildString(...template){
+  return `I like ${template.join(', ')}!`;
+}
+
+// 12. Welcome to the City
+// Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+
+// Example:
+
+// ['John', 'Smith'], 'Phoenix', 'Arizona'
+// This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+
+function sayHello( name, city, state ) {
+return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+}
+
+// 13. Dollars and Cents
+// DESCRIPTION:
+// The company you work for has just been awarded a contract to build a payment gateway. In order to help move things along, you have volunteered to create a function that will take a float and return the amount formatting in dollars and cents.
+
+// 39.99 becomes $39.99
+
+// The rest of your team will make sure that the argument is sanitized before being passed to your function although you will need to account for adding trailing zeros if they are missing (though you won't have to worry about a dangling period).
+
+// Examples:
+
+// 3 needs to become $3.00
+
+// 3.1 needs to become $3.10
+// Good luck! Your team knows they can count on you!
+
+function formatMoney(amount){
+  return `$${parseFloat(amount).toFixed(2)}`
+}
+
+
