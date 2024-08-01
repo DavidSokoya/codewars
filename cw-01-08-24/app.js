647c1bd8ad88ee80ac37486f98bea8d1133ad573
DavidSokoya <sokoya08@gmail.com>
2024-08-01 15:16:55 +0100

    Count of positives

--- cw-01-08-24/app.js
+++ cw-01-08-24/app.js
@@ -79,3 +79,24 @@ function countSheeps(sheep) {
   return presentSheep.length;
   
 }
+
+// 5. Count of positives / sum of negatives
+
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+function countPositivesSumNegatives(input) {
+  if (input === null || input.length === 0) {
+    return []
+  }
+  let positives = input.filter(e => e > 0).length
+  let negatives = input.filter(e => e < 0)
+  let sumNegative = negatives.reduce((acc, curr) => acc + curr, 0) 
+  return [positives, sumNegative];
+}
