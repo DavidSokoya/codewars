24911052a1cab55ce1bf14f79679ed27b8ee8687
DavidSokoya <sokoya08@gmail.com>
2024-08-01 15:16:06 +0100

    count the sheep

--- cw-01-08-24/app.js
+++ cw-01-08-24/app.js
@@ -59,3 +59,23 @@ function monkeyCount(n) {
   }
   return arr;
 }
+
+// 4. Counting sheep...
+
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+
+function countSheeps(sheep) {
+  let presentSheep = sheep.filter((e) => e === true);
+  
+  return presentSheep.length;
+  
+}
