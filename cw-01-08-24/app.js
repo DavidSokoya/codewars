f60fe5c27e102046a31dc75856e5cea639853fbc
DavidSokoya <sokoya08@gmail.com>
2024-08-01 23:20:40 +0100

    gravity flip

--- cw-01-08-24/app.js
+++ cw-01-08-24/app.js
@@ -100,3 +100,13 @@ function countPositivesSumNegatives(input) {
   let sumNegative = negatives.reduce((acc, curr) => acc + curr, 0) 
   return [positives, sumNegative];
 }
+
+// 1. Gravity Flip
+const flip=(d, a)=>{
+  if (d === 'R') {
+    return a.sort((a, b) => a - b)
+  } else if (d === 'L'){
+    return a.sort((a, b) => b - a)
+  }
+
+}
