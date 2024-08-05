5a5e909e7a765a40262ff822185cf42839350adc
DavidSokoya <sokoya08@gmail.com>
2024-08-05 16:34:13 +0100

    pick a set of first elements

--- cw-05-08-24/app.js
+++ cw-05-08-24/app.js
@@ -86,3 +86,20 @@ function gooseFilter (birds) {
     return birds.filter((bird) => !geese.includes(bird) )
  
 };
+
+// 5. pick a set of first elements
+
+// Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+
+// If n == 0 return an empty sequence []
+
+// Examples
+// var arr = ['a', 'b', 'c', 'd', 'e'];
+// first(arr) //=> ['a'];
+// first(arr, 2) //=> ['a', 'b']
+// first(arr, 3) //=> ['a', 'b', 'c'];
+// first(arr, 0) //=> [];
+
+function first(arr, n) {
+  return n !== undefined ? arr.slice(0, n) : arr.slice(0, 1)
+}
