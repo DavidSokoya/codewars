8a8cd412fd79dea39d2935e41b06a9713c17f423
DavidSokoya <sokoya08@gmail.com>
2024-08-06 10:53:31 +0100

    Is the date today

---
+++ cw-06-08-24/app.js
@@ -0,0 +1,14 @@
+// Is the date today
+
+// Write a simple function that takes as a parameter a date object and returns a boolean value representing whether the date is today or not.
+
+// Make sure that your function does not return a false positive by only checking the day.
+
+function isToday(date) {
+  const today = new Date();
+  return (
+    date.getFullYear() === today.getFullYear() &&
+    date.getMonth() === today.getMonth() &&
+    date.getDate() === today.getDate()
+  );
+}
