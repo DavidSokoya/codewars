f2251e8838f8eeebaa434706ab6a03cd23ec756c
DavidSokoya <sokoya08@gmail.com>
2024-08-07 12:42:06 +0100

    sort gift code

--- cw-07-08-24/app.js
+++ cw-07-08-24/app.js
@@ -75,3 +75,13 @@ function conjugate(verb){
       return { [verb]: conjugatedForms };
     }
   }
+
+// Sort the Gift Code
+//   Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+// Examples (Input -- => Output):
+// "abcdef"                      -- => "abcdef"
+// "pqksuvy"                     -- => "kpqsuvy"
+// "zyxwvutsrqponmlkjihgfedcba"  -- => "abcdefghijklmnopqrstuvwxyz"
+function sortGiftCode(code){
+  return code.split('').sort((a, b) => a.localeCompare(b)).join('');
+}
