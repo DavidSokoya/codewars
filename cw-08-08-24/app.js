7a292e6908a30995b184e318267bd785724f94e0
DavidSokoya <sokoya08@gmail.com>
2024-08-08 16:45:51 +0100

    boredom

--- cw-08-08-24/app.js
+++ cw-08-08-24/app.js
@@ -18,3 +18,35 @@ function doubleEveryOther(a) {
 }
 
 // The Office II - Boredom Score
+function boredom(staff){
+  let bordormScore = {
+    accounts: 1,
+    finance: 2,
+    canteen: 10,
+    regulation: 3,
+    trading: 6,
+    change: 6,
+    IS: 8,
+    retail: 5,
+    cleaning: 4,
+    'pissing about': 25
+  }
+  
+  let sentiment = '';
+  let staffValues = Object.values(staff)
+
+  if (staffValues.some((department) => bordormScore.hasOwnProperty(department)))
+  
+  {
+   score =  staffValues.map(value => bordormScore[value]).reduce((a, b) => a + b, 0)
+    
+    if (score <= 80){
+      sentiment = 'kill me now';
+    }else if (score <= 100){
+      sentiment = "i can handle this"
+    }else {
+      sentiment = 'party time!!'
+    }
+  }
+  return sentiment;
+}
