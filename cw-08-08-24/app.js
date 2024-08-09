115e2687a6a3f4ae17e52555a11c51ea5dc30609
DavidSokoya <sokoya08@gmail.com>
2024-08-09 18:00:10 +0100

    Strings

--- cw-08-08-24/app.js
+++ cw-08-08-24/app.js
@@ -50,3 +50,42 @@ function boredom(staff){
   }
   return sentiment;
 }
+
+const SCORES = {
+  accounts: 1,
+  finance: 2,
+  canteen: 10,
+  regulation: 3,
+  trading: 6,
+  change: 6,
+  IS: 8,
+  retail: 5,
+  cleaning: 4,
+  'pissing about': 25
+};
+
+function boredom(staff){
+  let val = Object.keys(staff).reduce((acc, cur) => acc + SCORES[staff[cur]], 0);
+    
+  if (val <= 80) return 'kill me now';
+  if (val > 100) return 'party time!!';
+  return 'i can handle this';
+}
+
+// const StreetFiter(height, weight, color, type) {
+//   this.height = height
+//   this.weight = weight
+//   this.color = color
+//   this.type = type 
+//   this.punch = function(){
+//     console.log("Punch")
+//   }
+//   this.upperCut = function(){
+//     console.log("Upper cut")
+//   }
+//   this.sumasult = function (){
+//     console.log("summer....")
+//   }
+// }
+
+// let john = new StreetFiter(27, 56, black, random)
